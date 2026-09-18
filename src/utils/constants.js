78735587920ee8a31398d39a3d8217e9da0c7a04
Firoz <146817644+Firozfs5@users.noreply.github.com>
2026-09-18 23:31:28 +0530

    update on constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,8 @@
 // export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = "/api";
+// export const BASE_URL =
+//   location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://devtiner-backend.onrender.com";
